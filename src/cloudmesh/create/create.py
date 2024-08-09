06960620639960a9ada280d1016ea23988cdf148
Harshad Pitkar <harshpitkar@gmail.com>
2024-08-09 14:56:14 -0400

    added code to create eks cluster, nodegroup and delete cluster - hp

--- src/cloudmesh/create/create.py
+++ src/cloudmesh/create/create.py
@@ -1,64 +1,144 @@
-class Create:
-    """
-    Create Class
+import yaml
+import boto3
+import time
 
-    This class provides basic functionality for the Create class.
+class deploy_cluster:
+    
+    def __init__(self,filename):
+    
+        with open(filename) as file:
+          config_data = yaml.load(file, Loader=yaml.FullLoader)
+        #return config_data
 
-    Methods:
-        - __init__(): Initialize the Create class.
-        - list(parameter): Print a message indicating a list operation with the provided parameter.
 
-    Usage:
-        create_instance = Create()
+        ########## Create Cluster 
 
-        # Initialize the Create class
-        create_instance.__init__()
+          cluster_name = (config_data.get('cluster')['clustername'])
+          role_arn = (config_data.get('cluster')['rolearn'])
 
-        # Perform a list operation
-        create_instance.list("example_parameter")
+          subnet_ids = (config_data.get('cluster')['subnetids'])
+          security_group_ids = (config_data.get('cluster')['securitygroupids'])
 
-    Author:
-        Your Name
-    """
+          response = self.create_eks_cluster(cluster_name, role_arn, subnet_ids, security_group_ids)
 
-    def __init__(self):
-        """
-        Initialize the Create class.
+          print(response)
 
-        This method prints an initialization message with the class name.
+        ## Sleep for 10 minutes to allow the cluster to be created
+          time.sleep(600)
 
-        Args:
-            None
+        ## Check if the cluster is active
 
-        Returns:
-            None
-        """
-        print("init {name}".format(name=self.__class__.__name__))
+          while  self.cluster_status(cluster_name) != 'ACTIVE':
+              print('Waiting for Cluster to be in active state')
+              time.sleep(60)
 
-    def list(self, parameter):
-        """
-        Perform a list operation.
+        ########### Node groups
 
-        This method prints a message indicating a list operation along with the provided parameter.
+          for i in config_data.get('nodegroups'):
+            print((i)['name'])
+            node_group_name = (i)['name']
+            instance_type = (i)['instanceType']
+            minSize = (i)['minSize']
+            maxSize = (i)['maxSize']
+            desiredSize = (i)['desiredCapacity']
+            amiType = (i)['amiType']
+            diskSize = (i)['volumeSize']
+            capacityType = (i)['capacityType']
+            subnet_ids = config_data.get('cluster')['subnetids']
+            role_arn = (i)['noderolearn']
+            response = self.create_node_group(cluster_name, node_group_name, instance_type, minSize, maxSize, desiredSize, amiType, diskSize, capacityType, subnet_ids, role_arn)
+            print(response)
 
-        Args:
-            parameter (str): The parameter for the list operation.
 
-        Returns:
-            None
-        """
-        print("list", parameter)
+    def create_eks_cluster(self, cluster_name, role_arn, subnet_ids, security_group_ids):
+        eks_client = boto3.client('eks')
+        
+        response = eks_client.create_cluster(
+            name=cluster_name,
+            roleArn=role_arn,
+            resourcesVpcConfig={
+                'subnetIds': subnet_ids,
+                'securityGroupIds': security_group_ids
+            }
+        )
+        
+        return response
 
-    def create(self, arguments):
-        """
-        Perform a create operation.
+###############
 
-        This method performs a create operation based on the provided arguments.
+    def create_node_group(self, cluster_name, node_group_name, instance_type, minSize, maxSize, desiredSize, amiType, diskSize, capacityType, subnet_ids, role_arn):
 
-        Args:
-            arguments (dict): The arguments for the create operation.
+        eks_client = boto3.client('eks')
 
-        Returns:
-            None
-        """
-        print("create", arguments)
+        #print(eks_client.describe_cluster(name=cluster_name))
+
+        response = eks_client.create_nodegroup(
+            clusterName=cluster_name,
+            nodegroupName=node_group_name,
+            scalingConfig={
+                'minSize': minSize,
+                'maxSize': maxSize,
+                'desiredSize': desiredSize
+            },
+            diskSize=diskSize,
+            subnets=subnet_ids,
+            instanceTypes=[
+                instance_type,
+            ],
+            amiType=amiType,
+            nodeRole=role_arn,
+            tags={
+                'clusterName' : cluster_name
+            },
+            updateConfig={
+                'maxUnavailable': 1,
+            },
+            capacityType=capacityType,
+        )
+
+        return response
+
+    def cluster_status(self, cluster_name):
+        eks_client = boto3.client('eks')
+        response = eks_client.describe_cluster(name=cluster_name)
+        return response['cluster']['status']
+
+    def delete_nodegroup(cluster_name, nodegroup_name):
+        eks_client = boto3.client('eks')
+        response = eks_client.delete_nodegroup(
+            clusterName=cluster_name,
+            nodegroupName=nodegroup_name
+        )
+        return response
+
+    def delete_cluster(cluster_name):
+        eks_client = boto3.client('eks')
+        response = eks_client.delete_cluster(
+            name=cluster_name
+        )
+        return response
+    
+########## Read YAML file
+
+
+### deploy cluster
+
+
+    # Usage:
+
+    #     # Initialize the Create class
+    #     create_instance.__init__()
+
+    #deploy_cluster('clu-config.yaml')
+
+    # delete node group
+
+    #deploy_cluster.delete_nodegroup('eks-4', 'ngrp2')
+
+    # delete cluster    
+
+    #deploy_cluster.delete_cluster('eks-4')
+
+    # Author:
+    #     Harshad Pitkar
+    # """

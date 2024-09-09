3186010bd2f727b39344691a4caa9fd9f043509d
Gregor von Laszewski <laszewski@gmail.com>
2024-09-08 20:35:07 -0400

    update env mac script

--- env-mac.sh
+++ env-mac.sh
@@ -1,6 +1,10 @@
+# dont forget to call deactivate before calling it
+deactivate
 rm -fr ~/ENVy
 python3 -m venv ~/ENVy
 source ~/ENVy/bin/activate
 pip install pip -U
 pip install cloudmesh-cmd5
+cms help
+make local
 cms help
